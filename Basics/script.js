8d5c3adf2a1750d25995c9d06f14b9bba3b9e1f3
Tanmay <tanmaykangule7271@gmail.com>
2025-11-27 14:44:55 +0530

    Started Backend Dev

---
+++ Basics/script.js
@@ -0,0 +1,16 @@
+// Arrays Method
+// Func and Objs
+// Func Return
+// Async Func
+
+var obj = {
+    name: "Tanmay",
+    age: 24,
+    hobbies: "Skating"
+}
+
+const abcd = () => {
+    return "Hello Wrodl"
+}
+
+var ans = abcd();
