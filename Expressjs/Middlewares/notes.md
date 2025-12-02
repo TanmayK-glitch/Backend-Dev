6f6d3413b053d712fad1f42fc69c8d2260fdc552
Tanmay <tanmaykangule7271@gmail.com>
2025-12-02 22:51:24 +0530

    Error Handling (Middlewares)

--- Expressjs/Middlewares/notes.md
+++ Expressjs/Middlewares/notes.md
@@ -1,3 +1,8 @@
+-- Middlewares
 - Middleware is basically used to do some things before ROUTES.
 - Before the main function (app.get) -> use this as Middleware -> app.use(function(req, res, next))..... takes 3 parameters
 - Whenever we use app.get function for middleware after end of function, we call next(); so the code execution gets transfered to the next block of code.
+
+-- Error Handling
+- app.use(function(err, req, res, next)) -> structure of Code.
+- 
--- Expressjs/Middlewares/script.js
+++ Expressjs/Middlewares/script.js
@@ -1,17 +1,62 @@
+// const express = require('express');
+// const app = express();
+
+// app.use(function (req, res, next) {
+//     console.log("Middleware Working!!!");
+//     next();
+// });
+
+// app.get('/', function (req, res) {
+//     res.send('Hello World');
+// });
+
+// app.get('/profile', function (req, res) {
+//     res.send('This is Tanmay');
+// });
+
+// app.listen(3000);
+
+// ---------------------Error Handling in Middlewares-----------------------
 const express = require('express');
 const app = express();
 
+// Normal Middleware
 app.use(function (req, res, next) {
-    console.log("Middleware Working!!!");
+    console.log("Normal Middleware!!");
     next();
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
+// Route 1
+app.get('/safe', function (req, res) {
+    res.send("Hello");
 });
 
-app.get('/profile', function (req, res) {
-    res.send('This is Tanmay');
+app.get('/danger', function (req, res, next) {
+    const userDoesNotExits = true;
+
+    if (userDoesNotExits) {
+        const error = new Error('User Not Found!!'); s
+        error.status = 400;
+        return next(error);
+    }
+    res.send('Never Visible!');
 });
 
-app.listen(3000);
+app.get('/boom', function (req, res) {
+    throw new Error('Something Crashed!!');
+});
+
+// Error Handling 
+app.use((err, req, res, next) => {
+    console.error('Error:', err.message);
+
+    const status = err.status || 500;
+    res.status(status).json({
+        error: {
+            message: err.message,
+            status: status,
+        }
+    });
+});
+
+app.listen(3000, () => console.log('Server on port 3000'));
