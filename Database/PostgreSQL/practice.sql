85df909a1d28756711cf6e68c1254e99fe8606f5
Tanmay <tanmaykangule7271@gmail.com>
2026-01-01 19:35:46 +0530

    Practiced: Having / Group By

--- Database/PostgreSQL/practice.sql
+++ Database/PostgreSQL/practice.sql
@@ -44,4 +44,51 @@
 -- ORDER BY stock ASC
 -- LIMIT 3
 
-SELECT SUM(quantity) AS sum_qty, AVG(subtotal) AS avg_subtotal FROM order_items
+-- SELECT SUM(quantity) AS sum_qty, AVG(subtotal) AS avg_subtotal FROM order_items
+
+-- SELECT category ,COUNT(stock) AS stock_count
+-- FROM products
+-- GROUP BY category
+
+-- SELECT customer_id, SUM(total_amount) AS total_amount
+-- FROM orders
+-- GROUP BY customer_id 
+-- ORDER BY customer_id DESC 
+
+-- SELECT SUM(total_amount) AS Total_Amount, EXTRACT(MONTH FROM order_date) AS Month
+-- FROM orders
+-- GROUP BY MONTH
+
+-- SELECT * FROM products
+-- WHERE category IN (
+--     SELECT category FROM products
+--     GROUP BY category
+--     HAVING COUNT(stock) > 2
+-- )
+
+-- Group orders by customer_id and show customers
+--  who have spent more than 1000 in total (using SUM).
+
+-- SELECT * FROM orders
+-- WHERE customer_id IN (
+--     SELECT customer_id FROM orders
+--     GROUP BY customer_id
+--     HAVING SUM(total_amount) > 1000
+-- )
+
+-- Group order_items by order_id and
+--  show orders with average subtotal greater than 200 (using AVG).
+
+-- SELECT * FROM order_items
+-- WHERE order_id IN (
+--     SELECT order_id FROM order_items
+--     GROUP BY order_id
+--     HAVING AVG(subtotal) > 200
+-- ) 
+
+SELECT * FROM products
+WHERE category IN (
+    SELECT category FROM products
+    GROUP BY category
+    HAVING SUM(stock) >= 500
+)
