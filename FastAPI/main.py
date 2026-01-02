0b3e7d25df8367293a76cee1c17e2ebf8a6d3a16
Tanmay <tanmaykangule7271@gmail.com>
2026-01-02 09:25:10 +0530

    Learned: Path Parameters

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -2,6 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+students = {
+    1: {
+        "name": "Tanmay",
+        "age": 19,
+        "isStudent": True
+    }
+}
+
 @app.get("/")
 def index():
     return {"name": "First Data"}
+
+@app.get("/student_info/{student_id}")
+def info(student_id: int):
+    return students[student_id]
