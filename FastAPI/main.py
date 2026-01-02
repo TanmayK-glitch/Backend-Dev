99e8e57e37ca31f2c25e49a30d4ae70264459860
Tanmay <tanmaykangule7271@gmail.com>
2026-01-02 18:57:45 +0530

    Learned: Query Parameters

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -4,8 +4,20 @@ app = FastAPI()
 
 students = {
     1: {
-        "name": "Tanmay",
+        "name": "tanmay",
         "age": 19,
+        "isStudent": True
+    },
+
+    2: {
+        "name": "krishna",
+        "age": 55,
+        "isStudent": False
+    },
+
+    3: {
+        "name": "pratik",
+        "age": 18,
         "isStudent": True
     }
 }
@@ -14,6 +26,16 @@ students = {
 def index():
     return {"name": "First Data"}
 
+# Path Parameter
 @app.get("/student_info/{student_id}")
 def info(student_id: int):
-    return students[student_id]
+    return students[student_id] #Or can be written like below also
+    # return students.get(student_id, {"error": "cannot fetch student info"})
+
+#Query Parameter
+@app.get("/student_name")
+def getName(name: str):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"error": "student not found"}
